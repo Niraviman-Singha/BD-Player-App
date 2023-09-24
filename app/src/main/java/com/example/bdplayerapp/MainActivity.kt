d1e8af7c0b60b79b8e423516c08114339e885657
niraviman-singha <niraviman15-11689@diu.edu.bd>
2023-09-24 16:41:35 +0600

    Backend Completed

--- app/src/main/java/com/example/bdplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/bdplayerapp/MainActivity.kt
@@ -15,8 +15,15 @@ class MainActivity : AppCompatActivity() {
 
         binding.playerRecycle.layoutManager = LinearLayoutManager(this)
         val playerList = listOf<PlayerModel>(
-            PlayerModel("Mushfiqur Rahman", resources.getString(R.string.player1), R.drawable.mushfiq)
-        )
+            PlayerModel("Shakib Al-Hasan", resources.getString(R.string.shakib), R.drawable.shakib),
+            PlayerModel("Tamim Iqbal", resources.getString(R.string.tamim), R.drawable.tamim),
+            PlayerModel("Mushfiqur Rahman", resources.getString(R.string.mushfiq), R.drawable.mushfiq),
+            PlayerModel("Mushfiqur Rahim", resources.getString(R.string.mustafizur), R.drawable.mustafiz),
+            PlayerModel("Mehidy Hasan Miraz", resources.getString(R.string.mehidy), R.drawable.miraj),
+            PlayerModel("Mahmudullah", resources.getString(R.string.mahmudullah), R.drawable.mahmudullah),
+            PlayerModel("Litton Das", resources.getString(R.string.liton), R.drawable.liton),
+
+            )
 
         binding.playerRecycle.adapter = PlayerAdapter(playerList,){
                 selectItem: PlayerModel -> listItemClicked(selectItem)
