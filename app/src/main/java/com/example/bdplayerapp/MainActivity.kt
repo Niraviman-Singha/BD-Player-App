bcc17055d879b86e2ffb0dba05f52aeee4a56771
niraviman-singha <niraviman15-11689@diu.edu.bd>
2023-09-24 16:09:52 +0600

    Design Comlete

--- app/src/main/java/com/example/bdplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/bdplayerapp/MainActivity.kt
@@ -17,15 +17,9 @@ class MainActivity : AppCompatActivity() {
         val playerList = listOf<PlayerModel>(
             PlayerModel("Mushfiqur Rahman", resources.getString(R.string.player1), R.drawable.mushfiq)
         )
-        val player2List = listOf<Player2Model>(
-            Player2Model("Tamim Iqbal", resources.getString(R.string.player2), R.drawable.tamim)
-        )
 
         binding.playerRecycle.adapter = PlayerAdapter(playerList,){
                 selectItem: PlayerModel -> listItemClicked(selectItem)
-        }
-        binding.player2Recycle.adapter = Player2Adapter(player2List,){
-                selectItem: Player2Model -> listItemClicked(selectItem)
         }
 
     }
@@ -37,11 +31,4 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun listItemClicked(player:Player2Model){
-        var intent = Intent(this@MainActivity, Player2Activity::class.java)
-        intent.putExtra("title", player.title)
-        intent.putExtra("details", player.details)
-        intent.putExtra("img", player.img)
-        startActivity(intent)
-    }
 }
--- app/src/main/java/com/example/bdplayerapp/Player2Model.kt
+++
@@ -1,3 +0,0 @@
-package com.example.bdplayerapp
-
-data class Player2Model(val title:String, val details:String, val img:Int)
